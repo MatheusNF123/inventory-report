be53501abc2e81f39aad7c6e16aa943e136ee6e6
Matheus <matheus_cs.nf@hotmail.com>
2023-01-31 16:35:42 -0300

    requisito 2 implementando uma class com metodo statico e retornando uma string no formato correto

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,18 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(lista: list[dict]):
+        dataF = min(lista, key=lambda x: x["data_de_fabricacao"])
+        dataV = min(lista, key=lambda x: x["data_de_validade"])
+        a = []
+        for i in lista:
+            a.append(i["nome_da_empresa"])
+
+        numeroEmpresa = Counter(a).most_common()[0][0]
+
+        return (
+            f"""Data de fabricação mais antiga: {dataF['data_de_fabricacao']}
+Data de validade mais próxima: {dataV['data_de_validade']}
+Empresa com mais produtos: {numeroEmpresa}""")
