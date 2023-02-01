afe9d1b331f5b94f856d84590c0d8c4df3e3b412
Matheus <matheus_cs.nf@hotmail.com>
2023-02-01 17:11:10 -0300

    refatorando a logica de abrir um arquivo xml

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -10,19 +10,7 @@ class XmlImporter(Importer):
 
         with open(path) as file:
             xml_file = file.read()
-            lista = []
             dict_xml = xmltodict.parse(xml_file)
-            for i in dict_xml["dataset"]["record"]:
-                dicts = {
-                    "id": i["id"],
-                    "nome_do_produto": i["nome_do_produto"],
-                    "nome_da_empresa": i["nome_da_empresa"],
-                    "data_de_fabricacao": i["data_de_fabricacao"],
-                    "data_de_validade": i["data_de_validade"],
-                    "numero_de_serie": i["numero_de_serie"],
-                    "instrucoes_de_armazenamento": i[
-                        "instrucoes_de_armazenamento"
-                    ],
-                }
-                lista.append(dicts)
-            return lista
+            list_dict_product = dict_xml["dataset"]["record"]
+
+            return list_dict_product
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -44,23 +44,10 @@ class OpenXML:
     def open(path):
         with open(path) as file:
             xml_file = file.read()
-            lista = []
             dict_xml = xmltodict.parse(xml_file)
-            for i in dict_xml["dataset"]["record"]:
-                dicts = {
-                    "id": i["id"],
-                    "nome_do_produto": i["nome_do_produto"],
-                    "nome_da_empresa": i["nome_da_empresa"],
-                    "data_de_fabricacao": i["data_de_fabricacao"],
-                    "data_de_validade": i["data_de_validade"],
-                    "numero_de_serie": i["numero_de_serie"],
-                    "instrucoes_de_armazenamento": i[
-                        "instrucoes_de_armazenamento"
-                    ],
-                }
-                lista.append(dicts)
+            list_dict_product = dict_xml["dataset"]["record"]
 
-            return lista
+            return list_dict_product
 
 
 class Inventory:
