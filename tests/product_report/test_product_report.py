e937ac79e1fae7c2c1b1ee378fb32b9aba918eb1
Matheus <matheus_cs.nf@hotmail.com>
2023-02-01 00:21:09 -0300

    implementando teste do metodo __repr da classe product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "FM500",
+        "ao abrigo de luz",
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto} fabricado em "
+        f"{product.data_de_fabricacao} por {product.nome_da_empresa} "
+        f"com validade até {product.data_de_validade} precisa ser armazenado "
+        f"{product.instrucoes_de_armazenamento}."
+    )
